a8ede55233c1ab56a36d42e9aa3dc7b5e55eb7c5
Navnath Reshmaji Chinchore <nath.chinchore@gmail.com>
2020-08-14 19:43:48 +0530

    added baase entity

--- pet-clinic-data/src/main/java/com/navnath/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/navnath/sfgpetclinic/model/Vet.java
@@ -1,13 +1,4 @@
 package com.navnath.sfgpetclinic.model;
 
 public class Vet extends Person {
-    private  Long vetId;
-    public Long getVetId() {
-        return vetId;
-    }
-
-    public void setVetId(Long vetId) {
-        this.vetId = vetId;
-    }
-
 }
