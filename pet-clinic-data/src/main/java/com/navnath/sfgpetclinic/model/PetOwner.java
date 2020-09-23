6600285cc10e7a43969f2393a8b79a80796207fb
Navnath Reshmaji Chinchore <nath.chinchore@gmail.com>
2020-09-23 15:39:56 +0530

    Create PetType, Visit entity. closes #1

--- pet-clinic-data/src/main/java/com/navnath/sfgpetclinic/model/PetOwner.java
+++ pet-clinic-data/src/main/java/com/navnath/sfgpetclinic/model/PetOwner.java
@@ -1,4 +1,15 @@
 package com.navnath.sfgpetclinic.model;
 
+import java.util.Set;
+
 public class PetOwner extends Person {
+    private Set<Pet> pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
