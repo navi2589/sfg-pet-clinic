834d7280a282a36025c63e605e62c98c294dd635
Navnath Reshmaji Chinchore <nath.chinchore@gmail.com>
2020-09-29 13:34:02 +0530

    Convert Owners to JPA Entities. Closes #11

--- pet-clinic-data/src/main/java/com/navnath/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/navnath/sfgpetclinic/model/Pet.java
@@ -1,11 +1,23 @@
 package com.navnath.sfgpetclinic.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "pets")
 public class Pet extends BaseEntity {
+    @Column(name = "name")
     private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "pet_type_id")
     private PetType petType;
+
+    @ManyToOne
+    @JoinColumn(name = "pet_owner_id")
     private PetOwner petOwner;
+
+    @Column(name = "birth_date")
     private LocalDate birthDate;
 
     public String getName() {
--- pet-clinic-data/src/main/java/com/navnath/sfgpetclinic/model/PetOwner.java
+++ pet-clinic-data/src/main/java/com/navnath/sfgpetclinic/model/PetOwner.java
@@ -1,13 +1,22 @@
 package com.navnath.sfgpetclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
-
+@Entity
+@Table(name = "pet_owner")
 public class PetOwner extends Person {
 
+    @Column(name = "address")
     private String address;
+
+    @Column(name = "city")
     private String city;
+
+    @Column(name = "telephone")
     private String telephone;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy="petOwner")
     private Set<Pet> pets = new HashSet<>();
 
     public String getAddress() {
