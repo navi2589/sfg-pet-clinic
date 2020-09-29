6c9143ccca423109a9a4762fa1ca7148d5f9344f
Navnath Reshmaji Chinchore <nath.chinchore@gmail.com>
2020-09-29 17:05:02 +0530

    Add Spring Data JPA Vet Service. Closes #16

---
+++ pet-clinic-data/src/main/java/com/navnath/sfgpetclinic/services/sdjpaservices/VetSDService.java
@@ -0,0 +1,48 @@
+package com.navnath.sfgpetclinic.services.sdjpaservices;
+
+import com.navnath.sfgpetclinic.model.Vet;
+import com.navnath.sfgpetclinic.repositories.VetRepository;
+import com.navnath.sfgpetclinic.services.VetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class VetSDService implements VetService {
+
+    private final VetRepository vetRepository;
+
+    public VetSDService(VetRepository vetRepository) {
+        this.vetRepository = vetRepository;
+    }
+
+    @Override
+    public Set<Vet> findAll() {
+        Set<Vet> vets = new HashSet<>();
+        vetRepository.findAll().forEach(vets::add);
+        return vets;
+    }
+
+    @Override
+    public Vet findById(Long aLong) {
+        return vetRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return vetRepository.save(object);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        vetRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        vetRepository.deleteById(aLong);
+    }
+}
