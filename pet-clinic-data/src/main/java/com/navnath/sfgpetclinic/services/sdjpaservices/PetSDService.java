3d8825da2b98d0b2183df7d3c8131e5c5e9a664d
Navnath Reshmaji Chinchore <nath.chinchore@gmail.com>
2020-10-09 13:43:50 +0530

    Add Spring Data JPA Pet  Service. Closes #18

---
+++ pet-clinic-data/src/main/java/com/navnath/sfgpetclinic/services/sdjpaservices/PetSDService.java
@@ -0,0 +1,46 @@
+package com.navnath.sfgpetclinic.services.sdjpaservices;
+
+import com.navnath.sfgpetclinic.model.Pet;
+import com.navnath.sfgpetclinic.repositories.PetRepository;
+import com.navnath.sfgpetclinic.services.PetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class PetSDService implements PetService {
+    private final PetRepository petRepository;
+    public PetSDService(PetRepository petRepository) {
+        this.petRepository = petRepository;
+    }
+
+    @Override
+    public Set<Pet> findAll() {
+        Set<Pet> pets = new HashSet<>();
+        petRepository.findAll().forEach(pet-> pets.add(pet));
+        return pets;
+    }
+
+    @Override
+    public Pet findById(Long aLong) {
+        return petRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return petRepository.save(object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        petRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        petRepository.deleteById(aLong);
+    }
+}
